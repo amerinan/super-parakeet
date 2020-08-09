e96ab7319e472b8e4252d463b4790b405375d3c9
Christian Kinsley <carpediem.ck@gmail.com>
2020-08-09 09:43:05 -0400

    Completed Version

--- Magic8ball/magic8ball.Designer.cs
+++ Magic8ball/magic8ball.Designer.cs
@@ -28,13 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAsk = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Location = new System.Drawing.Point(12, 29);
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.Size = new System.Drawing.Size(640, 20);
+            this.txtQuestion.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ask a question:";
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(12, 82);
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ReadOnly = true;
+            this.txtResponse.Size = new System.Drawing.Size(640, 20);
+            this.txtResponse.TabIndex = 2;
+            this.txtResponse.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Response:";
+            // 
+            // btnAsk
+            // 
+            this.btnAsk.Location = new System.Drawing.Point(16, 130);
+            this.btnAsk.Name = "btnAsk";
+            this.btnAsk.Size = new System.Drawing.Size(75, 23);
+            this.btnAsk.TabIndex = 1;
+            this.btnAsk.Text = "&Ask";
+            this.btnAsk.UseVisualStyleBackColor = true;
+            this.btnAsk.Click += new System.EventHandler(this.btnAsk_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(203, 130);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.Location = new System.Drawing.Point(390, 129);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayAgain.TabIndex = 3;
+            this.btnPlayAgain.Text = "&Play Again";
+            this.btnPlayAgain.UseVisualStyleBackColor = true;
+            this.btnPlayAgain.Click += new System.EventHandler(this.btnPlayAgain_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnQuit.Location = new System.Drawing.Point(577, 129);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 4;
+            this.btnQuit.Text = "&Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // magic8ball
+            // 
+            this.AcceptButton = this.btnAsk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.CancelButton = this.btnQuit;
+            this.ClientSize = new System.Drawing.Size(672, 175);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAsk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtQuestion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "magic8ball";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Magic 8 Ball";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAsk;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnPlayAgain;
+        private System.Windows.Forms.Button btnQuit;
     }
 }
 
--- Magic8ball/magic8ball.cs
+++ Magic8ball/magic8ball.cs
@@ -12,9 +12,72 @@ namespace Magic8ball
 {
     public partial class magic8ball : Form
     {
+        private List<string> answers = new List<string>()
+        {
+            "As I see it, yes.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don’t count on it.",
+            "It is certain.",
+            "It is decidedly so.",
+            "Most likely.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Outlook good.",
+            "Reply hazy, try again.",
+            "Signs point to yes.",
+            "Very doubtful.",
+            "ithout a doubt.",
+            "Yes.",
+            "Yes – definitely.",
+            "You may rely on it."
+
+        };
         public magic8ball()
         {
             InitializeComponent();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtQuestion.Clear();
+            
+        }
+
+        private void btnAsk_Click(object sender, EventArgs e)
+        {
+            if(txtQuestion.Text != "")
+            {
+                txtResponse.Clear();
+                response();
+            }
+            else
+            {
+                MessageBox.Show("Please ask a question.");
+                txtQuestion.Focus();
+                txtResponse.Clear();
+            }
+        }
+
+        private void response()
+        {
+            var random = new Random();
+            int index = random.Next(answers.Count);
+            txtResponse.Text = answers[index];
+        }
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            txtQuestion.Clear();
+            txtResponse.Clear();
         }
     }
 }
